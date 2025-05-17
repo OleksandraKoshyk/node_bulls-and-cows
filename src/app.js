daaf00d6adef1304a2cb2db3b491c5b5659e6d6c
Oleksandra Koshyk <sakoshik1984@gmail.com>
2025-05-17 16:21:38 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+
+const { generateRandomNumber } = require('./modules/generateRandomNumber.js');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput.js');
+const { getBullsAndCows } = require('./modules/getBullsAndCows.js');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question(
+  'Guess randomly generates a number of 4 different digits? ',
+  (userInput) => {
+    const numberToGuess = generateRandomNumber();
+
+    if (checkIsValidUserInput(userInput)) {
+      // eslint-disable-next-line no-console
+      console.log(getBullsAndCows(userInput, numberToGuess));
+    } else {
+      throw new Error('Invali input');
+    }
+    terminal.close();
+  },
+);
